823a2545e2f3481ed3994045cf6b4d7dd6ef8cba
Luu Truong <truonglv@outlook.com>
2021-09-17 09:14:54 +0700

    Update demo

--- demo/components/ReactionButton/helpers.ts
+++ demo/components/ReactionButton/helpers.ts
@@ -1,3 +1,7 @@
 export function isValidObject(obj: any): boolean {
   return obj !== null && typeof obj === 'object';
 }
+
+export function isNumber(val: any): boolean {
+  return typeof val === 'number';
+}
